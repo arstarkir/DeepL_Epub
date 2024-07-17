6d4e633dc44789545386f9bbd754a69a61d6f08d
arstarkir <arstarkir@gmail.com>
2024-07-17 08:29:10 -0400

    First

---
+++ translator/translator/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace translator
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            textBox1 = new TextBox();
+            label3 = new Label();
+            button1 = new Button();
+            comboBox1 = new ComboBox();
+            checkedListBox1 = new CheckedListBox();
+            button2 = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(104, 54);
+            label1.Name = "label1";
+            label1.Size = new Size(19, 15);
+            label1.TabIndex = 1;
+            label1.Text = "To";
+            label1.Click += label1_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(66, 6);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(100, 23);
+            textBox1.TabIndex = 2;
+            textBox1.Text = "";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(12, 9);
+            label3.Name = "label3";
+            label3.Size = new Size(48, 15);
+            label3.TabIndex = 3;
+            label3.Text = "API KEY";
+            label3.Click += label3_Click;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(12, 51);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 4;
+            button1.Text = "Add File";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // comboBox2
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(129, 48);
+            comboBox1.Name = "comboBox2";
+            comboBox1.Size = new Size(121, 23);
+            comboBox1.TabIndex = 0;
+            comboBox1.SelectedIndexChanged += comboBox1_SelectedIndexChanged;
+            // 
+            // checkedListBox1
+            // 
+            checkedListBox1.FormattingEnabled = true;
+            checkedListBox1.Items.AddRange(new object[] { "Nothing" });
+            checkedListBox1.Location = new Point(104, 86);
+            checkedListBox1.Name = "checkedListBox1";
+            checkedListBox1.Size = new Size(551, 328);
+            checkedListBox1.TabIndex = 5;
+            checkedListBox1.SelectedIndexChanged += checkedListBox1_SelectedIndexChanged;
+            checkedListBox1.Items.Clear();
+            // 
+            // button2
+            // 
+            button2.Location = new Point(567, 378);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 23);
+            button2.TabIndex = 6;
+            button2.Text = "I'm Done";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button2);
+            Controls.Add(checkedListBox1);
+            Controls.Add(button1);
+            Controls.Add(label3);
+            Controls.Add(textBox1);
+            Controls.Add(comboBox1);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+        private Label label1;
+        private TextBox textBox1;
+        private Label label3;
+        private Button button1;
+        private ComboBox comboBox1;
+        private CheckedListBox checkedListBox1;
+        private Button button2;
+    }
+
+}
---
+++ translator/translator/ItemDisplay.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace translator
+{
+    public class ItemDisplay<TValue>
+    {
+        private readonly string m_displayText;
+
+        public ItemDisplay(TValue value, String displayText)
+        {
+            this.Value = value;
+            m_displayText = displayText;
+        }
+
+        public TValue Value { get; set; }
+
+        public override string ToString()
+        {
+            return m_displayText;
+        }
+    }
+}
